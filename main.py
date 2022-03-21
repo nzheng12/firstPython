3cfe3654dd29a5c872eaf40546d36723bdde26e9
Nan Zheng <nan.zheng@SVUSLP00404.local>
2022-03-21 12:14:53 -0400

    added while loop

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import random
+
 
 result = 24
 name_of_unit = " hours"
 
 
-def days_to_units(num_of_days):
-    if num_of_days > 0:
-        return str(num_of_days) + " days are " + str(num_of_days * result) + str(name_of_unit)
+def days_to_units(num_of_days_element):
+    return str(num_of_days_element) + " days are " + str(num_of_days_element * result) + str(name_of_unit)
+
+
+def validate_and_execute():
+    if str(user_input).isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("value can't be 0")
     else:
-        return "Must use positive numbers"
+        print("not valid")
 
 
-user_input = input("enter a number of days\n")
-calculated_value = days_to_units(user_input)
-print(calculated_value)
+user_input = ""
+while user_input != "exit":
+    user_input = raw_input("enter a number of days\n")
+    for num_of_days_elements in (user_input.split()):
+        validate_and_execute()
